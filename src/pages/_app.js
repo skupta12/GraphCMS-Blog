af9a549d913fa9f51d9c2f7028f63545b43dc507
skupta12 <tosya2001@gmail.com>
2023-03-28 15:34:44 +0300

    _

---
+++ src/pages/_app.js
@@ -0,0 +1,12 @@
+import "@/styles/globals.scss";
+import React, { useEffect, useState } from 'react';
+import { Layout } from "../components";
+
+
+export default function App({ Component, pageProps }) {
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
+}
